8fbef6e6eb4df13d96ff4ab40f3ecbde719679b0
Manuel Guzmán <maguzdev@gmail.com>
2025-12-04 09:32:23 -0500

    feat: agregar soporte para PrimeIcons y componente Alerta, mejorar gestión de presupuesto

---
+++ src/components/Alerta.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-4 mb-4 rounded-lg relative" role="alert">
+        <slot></slot>
+    </div>
+</template>
--- src/components/Presupuesto.vue
+++ src/components/Presupuesto.vue
@@ -1,17 +1,49 @@
-<script setup></script>
+<script setup>
+	import { ref } from "vue";
+	import Alerta from "./Alerta.vue";
+	const presupuesto = ref('');
+    const mensajeError = ref('');
+
+	const añadirPresupuesto = () => {
+		if (presupuesto.value <= 0 || isNaN(presupuesto.value)) {
+            mensajeError.value = 'El presupuesto debe ser un número válido mayor que cero.';
+			setTimeout(() => {
+				mensajeError.value = '';
+			}, 3000);
+			return;
+		}
+
+		console.log("Presupuesto añadido:", presupuesto.value);
+	};
+</script>
 
 <template>
-    <form>
-        <div>
-            <label for="presupuesto" class="block text-gray-700 text-xl mb-4 text-center">Definir Presupuesto</label>
-            <input 
-                id="presupuesto"
-                type="number"
-                class="w-full p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-center text-xl"
-                placeholder="Añade tu presupuesto"
-            />
-        </div>
+	<form @submit.prevent="añadirPresupuesto">
+		<Alerta v-if="mensajeError">
+			<p class="text-center text-lg">
+				<i class="pi pi-exclamation-circle mr-2"></i>
+                {{ mensajeError }}
+			</p>
+		</Alerta>
+		<div>
+			<label
+				for="presupuesto"
+				class="block text-gray-700 text-xl mb-4 text-center"
+				>Definir Presupuesto</label
+			>
+			<input
+				id="presupuesto"
+				type="number"
+				class="w-full p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-center text-xl"
+				placeholder="Añade tu presupuesto"
+				v-model.number="presupuesto"
+			/>
+		</div>
 
-        <input type="submit" value="Añadir" class="mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 cursor-pointer w-full shadow-md transition-colors duration-300 ease-in-out" />
-    </form>
+		<input
+			type="submit"
+			value="Añadir"
+			class="mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 cursor-pointer w-full shadow-md transition-colors duration-300 ease-in-out"
+		/>
+	</form>
 </template>
