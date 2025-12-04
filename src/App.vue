5dc235a27f0ff1635c4e2f14df5feded051ea202
Manuel Guzmán <maguzdev@gmail.com>
2025-12-04 08:36:48 -0500

    feat: agregar componente Presupuesto y estructura inicial en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
-	
+	import Presupuesto from "./components/Presupuesto.vue";
 </script>
 
 <template>
-	
+	<div class="mx-auto">
+		<header class="bg-blue-500 px-10">
+			<h1 class="pt-8 text-white text-center text-4xl font-bold">
+				Planificador de Gastos
+			</h1>
+			<div
+				class="mx-auto max-w-3xl transform translate-y-14 p-14 bg-white shadow-lg rounded-lg"
+			>
+				<Presupuesto />
+			</div>
+		</header>
+	</div>
 </template>
---
+++ src/components/Presupuesto.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+    <form>
+        <div>
+            <label for="presupuesto" class="block text-gray-700 text-xl mb-4 text-center">Definir Presupuesto</label>
+            <input 
+                id="presupuesto"
+                type="number"
+                class="w-full p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-center text-xl"
+                placeholder="Añade tu presupuesto"
+            />
+        </div>
+
+        <input type="submit" value="Añadir" class="mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 cursor-pointer w-full shadow-md transition-colors duration-300 ease-in-out" />
+    </form>
+</template>
